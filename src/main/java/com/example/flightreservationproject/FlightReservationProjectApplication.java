7d9625052b8b5fef23d62df340df783f88f1418b
parkhoon01 <qkrgns0514@naver.com>
2025-02-20 23:32:35 +0900

    [fr-project]branch

---
+++ src/main/java/com/example/flightreservationproject/FlightReservationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.flightreservationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightReservationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightReservationProjectApplication.class, args);
+    }
+
+}
